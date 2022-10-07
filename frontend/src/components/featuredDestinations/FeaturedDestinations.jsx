7bc6700d5ae9011edb5f753a9db3ceb9c1032d08
Menta <gabrielmaxss@outlook.com>
2022-10-07 15:35:28 -0300

    feature: reactive front components

--- frontend/src/components/featuredDestinations/FeaturedDestinations.jsx
+++ frontend/src/components/featuredDestinations/FeaturedDestinations.jsx
@@ -1,43 +1,30 @@
+import useFetch from "../../hooks/useFetch";
 import "./featuredDestinations.css";
 
 const FeaturedDestinations = () => {
-	return (
-		<div className="featuredDestinations">
-			<div className="featuredDestinationsItem">
-				<img
-					src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
-					alt="Dublin"
-					className="featuredDestinationsImg"
-				/>
-				<div className="featuredDestinationsTitles">
-					<h1>Dublin</h1>
-					<h2>123 properties</h2>
-				</div>
-			</div>
 
-			<div className="featuredDestinationsItem">
-				<img
-					src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o="
-					alt="Reno"
-					className="featuredDestinationsImg"
-				/>
-				<div className="featuredDestinationsTitles">
-					<h1>Reno</h1>
-					<h2>533 properties</h2>
-				</div>
+	const { data, loading, error } = useFetch('http://localhost:8800/hotels?featured=true')
+
+	const featuredCities = data;
+
+	return (
+		<>
+			<div className="featuredDestinations">
+				{!loading && featuredCities.map((d, idx) => {
+					if (d.photos.length) return (
+						<>
+							<div className="featuredDestinationsItem" key={`${d?._id}${idx}`}>
+								<img src={d?.photos[0]} alt={d?.city} className='featuredDestinationsImg' />
+								<div className="featuredDestinationsTitles">
+									<h2>{d.city}</h2>
+									<h3>Booked {Math.ceil(Math.random() * 1000)} times recently</h3>
+								</div>
+							</div>
+						</>
+					)
+				})}
 			</div>
-			<div className="featuredDestinationsItem">
-				<img
-					src="https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o="
-					alt="Austin"
-					className="featuredDestinationsImg"
-				/>
-				<div className="featuredDestinationsTitles">
-					<h1>Austin</h1>
-					<h2>532 properties</h2>
-				</div>
-			</div>
-		</div>
+		</>
 	);
 };
 
